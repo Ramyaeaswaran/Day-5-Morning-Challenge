c2a87f9c1ade6ad63c81d286ab873a22ec53b603
Ramyaeaswaran <ramyaeaswaran77@gmail.com>
2019-10-04 13:02:32 +0530

    Day5_mrng_challenge

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+import 'dart:core';
 // Challenge One:
 // Create a function 'squarePatch' that takes an integer and prints an n x n square solely consisting of the integer n.
 //squarePatch(3) should print
@@ -6,7 +8,19 @@
 //    [3, 3, 3],
 //    [3, 3, 3]
 //  ]
-//
+List squarePatch(int n){
+  List values=[];
+ for(int i=1;i<=n;i++)
+   {
+     List val=[];
+     for(int j=1;j<=n;j++){
+       val.add(n);
+     }
+     values.add(val);
+   //  print(' ');
+   }
+  print(values);
+}
 //squarePatch(5) should print
 //  [
 //    [5, 5, 5, 5, 5],
@@ -22,6 +36,14 @@
 //  ]
 //
 // squarePatch(0) should print []
+printBoard(List<List<String>> board)  {
+  print('  A   B   C');
+  print('1 ${board[0][0]} |   |  ${board[0][2]}');
+  print(' ---+---+---');
+  print('2   | x |');
+  print(' ---+---+---');
+  print('3 x |   |') ;
+}
 
 main() {
 
@@ -39,6 +61,11 @@ main() {
   // 2    | X |
   //   ---+---+---
   // 3  X |   |
+ // squarePatch(3);
+ // squarePatch(5);
+  squarePatch(3);
+  squarePatch(5);
+  squarePatch(1);
 
   board[0][2] = 'O';
   printBoard(board);
